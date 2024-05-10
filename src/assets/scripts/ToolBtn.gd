3720774f5094976004ad15828202271508d2d05a
monoAI <46586720+monoai@users.noreply.github.com>
2024-05-10 17:23:07 +0800

    Modified tool buttons Tool buttons are now instanced scenes so that when modified, i wouldnt need to modify each individual one.

--- src/assets/scripts/ToolBtn.gd
+++ src/assets/scripts/ToolBtn.gd
@@ -1,22 +1,32 @@
 extends Button
 
+export var toolName : String
+export var toolDesc : String
 onready var price = basePrice
 export var basePrice : int
 export var amount : int
 
 # Get nodes
 onready var game = get_node("/root/Main")
-onready var priceTxt = $HBoxContainer/VBoxContainer/Price
+onready var nameTxt = $PanelContainer/HBoxContainer/VBoxContainer/Name
+onready var descTxt = $PanelContainer/HBoxContainer/VBoxContainer/Description
+onready var priceTxt = $PanelContainer/HBoxContainer/VBoxContainer/Price
+onready var amountTxt = $PanelContainer/Amount
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var _err = self.connect("pressed", self, "_on_Button_pressed")
 	if(_err):
 		push_error("[ERROR] - Button " + self.name + " not connected to a method properly.")
+	nameTxt.text = toolName
+	descTxt.text = toolDesc
 	pass
 
 func updateLabels():
+	# print(priceTxt)
+	# var priceTxt = $PanelContainer/HBoxContainer/VBoxContainer/Price
 	priceTxt.text = str(self.price) + " Pokes"
+	amountTxt.text = str(self.amount)
 
 func _on_Button_pressed():
 	purchase()
@@ -25,6 +35,7 @@ func purchase():
 	if(game.pokes >= self.price):
 		amount+=1
 		game.Spend(price)
+		game.updateLabels()
 		self.price = recalculatePrice()
 		updateLabels()
 
